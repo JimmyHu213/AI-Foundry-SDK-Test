d2abee3800a2915b7205c42a2393d4a93d5d833a
jimmyhu213 <jiemingh213@gmail.com>
2025-10-17 14:51:49 +1000

    init: set up main

---
+++ main.py
@@ -0,0 +1,9 @@
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+
+...
+
+project_endpoint = "https://......"
+project_client = AIProjectClient(
+    credential=DefaultAzureCredential(), endpoint=project_endpoint
+)
